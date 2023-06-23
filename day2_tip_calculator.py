c06ff0927c311bf12d950a2bedfe4dd3a0c2555e
21parthh <parthdeore2111@gmail.com>
2023-06-23 23:57:59 +0530

    day 3

--- day2_tip_calculator.py
+++ day2_tip_calculator.py
@@ -1,12 +1,10 @@
-print("Welcome to the tip Calculator")
-bill = float(input("What was the total bill? Rs"))
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill? RS."))
 people = int(input("How many people to split the bill? "))
-perc = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+perc = int(input("What percentage tip would you like to give? 10, 12, 15"))
 
-tip = (bill*perc)/100
+tip = (bill * perc)/100
+amount = tip + bill
+pay = amount/people
 
-final_amount = bill + tip
-
-pay = final_amount / people
-
-print(f"Each person should pay: Rs{pay}")                  
+print("Each person should pay: Rs",round(pay,2))
